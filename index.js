b3bacdd612a81e48f4126695b7ffa1e71f46041f
lovek-dev <lovekumar0412@gmail.com>
2026-02-13 01:56:43 +0000

    test5

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 import 'dotenv/config';
 import express from "express";
 import { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder, ActivityType } from "discord.js";
